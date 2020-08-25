1897e127d0f035ea2a48f191246fd3da093d2170
Maik Broisler <maikbroislerdevs@gmail.com>
2020-08-25 13:39:14 -0300

    GoStack | Project GoRestaurant with ReactJS

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import api from '../../services/api';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
@@ -28,10 +28,17 @@ const Food: React.FC<IProps> = ({
 
   async function toggleAvailable(): Promise<void> {
     // TODO UPDATE STATUS (available)
+    await api.put(`/foods/${food.id}`,{
+      ...food,
+      available: !isAvailable,
+    })
+    
+    setIsAvailable(!isAvailable);
   }
 
   function setEditingFood(): void {
     // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+    handleEditFood(food);
   }
 
   return (
